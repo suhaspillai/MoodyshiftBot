2093b0e97353a9aa6eb9c8379418d5212cafaba1
thang239 <thanghnbk@gmail.com>
2017-02-11 23:00:50 -0500

    midnight

--- moodyshift/getemotion/urls.py
+++ moodyshift/getemotion/urls.py
@@ -1,8 +1,10 @@
 # howdy/urls.py
-from django.conf.urls import url
+from django.conf.urls import  url
 from getemotion import views
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url(r'^about/$', views.AboutPageView.as_view()), # Add this /about/ route
+    url(r'^list/$', list, name='list')
 ]
--- moodyshift/getemotion/views.py
+++ moodyshift/getemotion/views.py
@@ -1,6 +1,12 @@
 # howdy/views.py
 from django.shortcuts import render
 from django.views.generic import TemplateView
+# from .forms import UploadFileForm
+from django.shortcuts import render
+
+from getemotion.models import Document
+from getemotion.forms import DocumentForm
+# from django.views.decorators.csrf import csrf_exempt
 
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
@@ -9,3 +15,31 @@ class HomePageView(TemplateView):
 # Add this view
 class AboutPageView(TemplateView):
     template_name = "about.html"
+
+# def handleupload(request):
+# 	if request.POST:
+# 		form = UploadFileForm(request.POST, request.FILES)
+def list(request):
+    # Handle file upload
+
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        print form
+        if form.is_valid():
+            newdoc = Document(docfile=request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return HttpResponseRedirect(reverse('list'))
+    else:
+        form = DocumentForm()  # A empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+
+    # Render list page with the documents and the form
+    return render(
+        request,
+        'list.html',
+        {'documents': documents, 'form': form}
+    )
